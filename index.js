3a0da05e1472667a3d176ca3f5400ad319767073
Mauricio Hernández <mauricioantoni@gmail.com>
2021-02-14 10:56:00 -0500

    archivos de prueba

---
+++ index.js
@@ -0,0 +1,33 @@
+
+let categoria = document.getElementById("categorias")
+
+
+let longitud = function () {
+    alert("longitud")
+    window.location="./longitud.html";
+}
+
+let elegirCategoia = function () {
+    let selectedIndex = categoria.selectedIndex
+    console.log("seleccionó",selectedIndex)
+
+    switch (selectedIndex) {
+        case 1: longitud()
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        
+        default:
+            alert("no seleccionó ninguno")
+            break;
+    }
+
+    
+}
+
+categoria.addEventListener("change",elegirCategoia)
